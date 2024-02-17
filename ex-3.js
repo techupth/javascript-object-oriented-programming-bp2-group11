53759d2912ed9d0445d9382cd9818b19018646cf
Phuree Limphornpugdee <phureelimphornpugdee@Phurees-MacBook-Air.local>
2024-02-17 17:15:26 +0700

    Excercise object oriented programming by Tar

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,89 @@
 //  Start coding here
+class User{
+    constructor(id,name,email)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+};
+
+class postList{
+    constructor(posts)
+    {
+        this.posts = posts;
+    };
+    addPost()
+    {
+        return this.posts.push();
+    };
+    sharePost()
+    {
+        console.log(`You've shared post ${title} to your friend.`);
+    }
+};
+
+class Post{
+    constructor(id,title,content,comment)
+    {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.comment = comment;
+    };
+    addComment()
+    {
+        return this.comment.push()
+    }
+};
+
+class Comment{
+    constructor(id,content,createdBy,like)
+    {
+        this.id = id;
+        this.content = content;
+        this.createdBy = createdBy;
+        this.like = like;
+    };
+    addLike()
+    {
+        return this.like = this.like + 1
+    }
+};
+class Facebook{
+    constructor(groupList,pageList)
+    {
+        this.groupList = groupList;
+        this.pageList = pageList;
+    };
+    addGroup()
+    {
+        return this.groupList.push();
+    };
+    addPage()
+    {
+        return this.pageList.push();
+    };
+};
+class FacebookPage{
+    constructor(name)
+    {
+        this.name = name;
+    };
+};
+class FacebookGroup{
+    constructor(name)
+    {
+        this.name = name;
+    };
+};
+class Notification{
+    constructor(id)
+    {
+        this.id = id;
+    };
+    send()
+    {
+        console.log(`Notification: ${createdBy} has just commented on this post—${title}`);
+    }
+};
